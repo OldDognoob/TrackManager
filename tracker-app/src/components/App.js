963c7d3e288beb1b0771b22768026badbaf7ffad
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-07 21:44:59 +0200

    create-app

---
+++ tracker-app/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "../App.css";
+import Header from "./Header";
+
+const App=()=>{
+    return(
+        <div className="container">
+            <div className="app-wrapper">
+                <Header/>
+            </div>
+        </div>
+    )
+}
+
+export default App;
